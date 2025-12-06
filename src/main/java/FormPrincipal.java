5b9856a16fe229b0a99fbf34b1fc9fd746715585
LFelipeRamos <142407961+LFelipeRamos@users.noreply.github.com>
2025-12-06 00:02:34 -0300

    Adiciona botão "Sair" ao menu principal e implementa funcionalidade de saída na classe FormPrincipal

--- src/main/java/FormPrincipal.java
+++ src/main/java/FormPrincipal.java
@@ -1,5 +1,8 @@
 
+// NetBeans IDE 27
+// Luiz Felipe Feranandes Ramos - RA 2767112
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -13,13 +16,21 @@ import javax.swing.JFrame;
 public class FormPrincipal extends javax.swing.JFrame {
     
     private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(FormPrincipal.class.getName());
+    //MÉTODO SINGLETON
+    private static FormPrincipal instancia;
+
+    public static FormPrincipal getInstance() {
+        if (instancia == null) {
+            instancia = new FormPrincipal();
+        }
+        return instancia;
+    }
 
     /**
      * Creates new form FormPrincipal
      */
-    public FormPrincipal() {
+    private FormPrincipal() {
         initComponents();
-        
     }
 
     /**
@@ -39,6 +50,7 @@ public class FormPrincipal extends javax.swing.JFrame {
         btnCadPaciente = new javax.swing.JMenuItem();
         btnCadDoador = new javax.swing.JMenuItem();
         btnCadMedico = new javax.swing.JMenuItem();
+        btnSair = new javax.swing.JMenuItem();
         btnRelatorios = new javax.swing.JMenu();
         btnRelPaciente = new javax.swing.JMenuItem();
         btnRelDoador = new javax.swing.JMenuItem();
@@ -83,6 +95,14 @@ public class FormPrincipal extends javax.swing.JFrame {
 
         btnMenu.add(cadMenu);
 
+        btnSair.setText("Sair");
+        btnSair.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSairActionPerformed(evt);
+            }
+        });
+        btnMenu.add(btnSair);
+
         barMenu.add(btnMenu);
 
         btnRelatorios.setText("Relatórios");
@@ -109,37 +129,56 @@ public class FormPrincipal extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(82, 82, 82)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(90, Short.MAX_VALUE)
                 .addComponent(jLabel2)
-                .addContainerGap(88, Short.MAX_VALUE))
+                .addGap(80, 80, 80))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(81, 81, 81)
+                .addGap(102, 102, 102)
                 .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(156, Short.MAX_VALUE))
+                .addContainerGap(135, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnCadMedicoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCadMedicoActionPerformed
-        // TODO add your handling code here:
+        FormMedico.getInstance().setVisible(true);
     }//GEN-LAST:event_btnCadMedicoActionPerformed
 
     private void btnCadDoadorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCadDoadorActionPerformed
-        // TODO add your handling code here:
+        FormDoador.getInstance().setVisible(true);
     }//GEN-LAST:event_btnCadDoadorActionPerformed
 
     private void btnCadPacienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCadPacienteActionPerformed
-        // TODO add your handling code here:
+        FormPaciente.getInstance().setVisible(true);
     }//GEN-LAST:event_btnCadPacienteActionPerformed
 
     private void btnRelPacienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRelPacienteActionPerformed
-        // TODO add your handling code here:
+        java.util.List<Paciente> list = BDHospital.getInstance().listarPacientes();
+        StringBuilder sb = new StringBuilder();
+        for (Paciente p : list) sb.append(p.listar()).append('\n');
+        JOptionPane.showMessageDialog(this, sb.length() == 0 ? "Nenhum paciente cadastrado" : sb.toString());
     }//GEN-LAST:event_btnRelPacienteActionPerformed
+
+    private void btnSairActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSairActionPerformed
+        sair();
+    }//GEN-LAST:event_btnSairActionPerformed
+
+    public void sair(){
+        int resp = JOptionPane.showConfirmDialog(
+            this,
+            "Deseja realmente sair?",
+            "Saída",
+            JOptionPane.YES_NO_OPTION
+        );
+        if(resp == JOptionPane.YES_OPTION){
+            dispose();
+        }
+    }
 
     /**
      * @param args the command line arguments
@@ -163,7 +202,7 @@ public class FormPrincipal extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(() -> new FormPrincipal().setVisible(true));
+        java.awt.EventQueue.invokeLater(() -> FormPrincipal.getInstance().setVisible(true));
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -176,6 +215,7 @@ public class FormPrincipal extends javax.swing.JFrame {
     private javax.swing.JMenuItem btnRelMedico;
     private javax.swing.JMenuItem btnRelPaciente;
     private javax.swing.JMenu btnRelatorios;
+    private javax.swing.JMenuItem btnSair;
     private javax.swing.JMenu cadMenu;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
